d6e1f196bb457a0f2cfef9987527a20d78c65d0f
HenryH054 <144753375+HenryH054@users.noreply.github.com>
2023-10-02 11:44:55 -0600

    added remove_element leet code runtine: 80th percentile, memory 98th percentile

--- remove_element.py
+++ remove_element.py
@@ -1,15 +1,31 @@
+"""Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+
+If all assertions pass, then your solution will be accepted.
+"""
+
 class Solution:
     def removeElement(nums: list[int], val: int) -> int:
-        x = len(nums)
+        k = 0
         i = 0
-        while i <= x or i == 0:
+        while i < len(nums):
             if nums[i] == val:
                 nums.pop(i)
-                x -= 1
-                i -= 1
             else:
+                k += 1
                 i += 1
-        return len(nums)
+        return k
     
 if __name__ == "__main__":
     print(Solution.removeElement([1], 1))
+    print(Solution.removeElement([], 5))
+    print(Solution.removeElement([3, 2, 6, 2], 2))
+    print(Solution.removeElement([1, 1, 1, 1], 1))        
